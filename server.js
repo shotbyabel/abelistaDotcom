148fe8020b39df7b00b8698ff1732fdad35f5273
kimo5532 <shotbyabel@gmail.com>
2015-10-02 16:33:25 -0700

    added bootsrap nav bar

---
+++ server.js
@@ -0,0 +1,80 @@
+var express      = require('express'),
+    path         = require('path'),
+    favicon      = require('serve-favicon'),
+    logger       = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser   = require('body-parser'),
+
+    users        = require('./routes/users');
+
+
+
+//Mongo running check
+require('net').connect(27017, 'locahost').on('error', function(){
+  console.log("He's name is Mongo and we kept him in a cage!");
+  process.exit(0);
+
+});
+
+//loading routes defined in /routes folder
+var routes    = require('./routes/index');
+
+//load mongoose and connect to our db
+
+var mongoose  = require('mongoose');
+mongoose.connect('mongodb://localhost/abelistaDotCom');
+
+
+// start running express, and save the configurations for the express
+// "app" with the variable `app`.
+var app       = express();    
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(logger('dev'));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
