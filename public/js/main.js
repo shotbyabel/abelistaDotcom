c9562459d64ef31a3260defff100c5796c3c8e56
kimo5532 <shotbyabel@gmail.com>
2015-10-13 02:10:38 -0700

    add jQuery easing animations

--- public/js/main.js
+++ public/js/main.js
@@ -4,3 +4,29 @@ $(document).ready(function() {
         );
   })
 });
+
+
+// ABOUT ME INTRO
+// jQuery EASING ANIMATION
+$('#ready').animate({left:'46%'},750, 'easeInOutQuad');
+$('#ready').fadeOut({top:-150},3000, 'easeInOutQuad');
+
+$('#willing').fadeTo(1500, 1.0);
+$('#willing').animate({right:'46%'},1000, 'easeInOutQuad');
+$('#willing').fadeOut({top:-150},3500, 'easeInOutQuad');
+
+// $('#and').fadeTo(5500, 1.0);
+// $('#and').animate({right:'46%'},1000, 'easeInOutQuad');
+// $('#and').fadeOut({top:-150},1500, 'easeInOutQuad');
+
+$('#abel').fadeTo(2000, 1.0);
+$('#abel').animate({left:'46%'},2000, 'easeInOutElastic');
+
+
+$('#fswd').fadeTo(3000, 1.0);
+$('#fswd').animate({top:150},1000,'easeOutBounce');
+
+
+
+
+
