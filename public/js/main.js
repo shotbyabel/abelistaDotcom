42318d4b7cf5d1c59b99219a025e6b2f091783eb
kimo5532 <shotbyabel@gmail.com>
2015-10-13 12:34:42 -0700

    work on media queries fix animation and play with GSAP

--- public/js/main.js
+++ public/js/main.js
@@ -11,20 +11,20 @@ $(document).ready(function() {
 $('#ready').animate({left:'46%'},750, 'easeInOutQuad');
 $('#ready').fadeOut({top:-150},3000, 'easeInOutQuad');
 
-$('#willing').fadeTo(1500, 1.0);
+$('#willing').fadeTo(1000, 1.0);
 $('#willing').animate({right:'46%'},1000, 'easeInOutQuad');
 $('#willing').fadeOut({top:-150},3500, 'easeInOutQuad');
 
-// $('#and').fadeTo(5500, 1.0);
-// $('#and').animate({right:'46%'},1000, 'easeInOutQuad');
-// $('#and').fadeOut({top:-150},1500, 'easeInOutQuad');
+$('#and').fadeTo(1750, 1.0);
+$('#and').animate({left:'46%'},1500, 'easeInOutQuad');
+$('#and').fadeOut({top:-150},1900, 'easeInOutQuad');
 
-$('#abel').fadeTo(2000, 1.0);
+$('#abel').fadeTo(2250, 1.0);
 $('#abel').animate({left:'46%'},2000, 'easeInOutElastic');
 
 
-$('#fswd').fadeTo(3000, 1.0);
-$('#fswd').animate({top:150},1000,'easeOutBounce');
+$('#fswd').fadeTo(3500, 1.0);
+$('#fswd').animate({top:145},2000,'easeOutBounce');
 
 
 
