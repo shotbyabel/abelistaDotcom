b5a995a946563b37c57e76a75e0247b4285945e1
kimo5532 <shotbyabel@gmail.com>
2016-12-24 23:39:07 -0800

    abelista PM updateo

--- public/js/main.js
+++ public/js/main.js
@@ -10,26 +10,27 @@ $(document).ready(function() {
 
 // ABOUT ME INTRO
 // jQuery EASING ANIMATION
-$('#ready').animate({left:'46%'},750, 'easeInOutQuad');
-$('#ready').fadeOut({top:-150},3000, 'easeInOutQuad');
+// $('#ready').animate({left:'46%'},750, 'easeInOutQuad');
+// $('#ready').fadeOut({top:-150},3000, 'easeInOutQuad');
 
-$('#willing').fadeTo(1000, 1.0);
-$('#willing').animate({right:'46%'},1000, 'easeInOutQuad');
-$('#willing').fadeOut({top:-150},3500, 'easeInOutQuad');
+// $('#willing').fadeTo(1000, 1.0);
+// $('#willing').animate({right:'46%'},1000, 'easeInOutQuad');
+// $('#willing').fadeOut({top:-150},3500, 'easeInOutQuad');
 
-$('#and').fadeTo(1750, 1.0);
-$('#and').animate({left:'46%'},1500, 'easeInOutQuad');
-$('#and').fadeOut({top:-150},1900, 'easeInOutQuad');
+// $('#and').fadeTo(1750, 1.0);
+// $('#and').animate({left:'46%'},1500, 'easeInOutQuad');
+// $('#and').fadeOut({top:-150},1900, 'easeInOutQuad');
 
-$('#abel').fadeTo(2250, 1.0);
-$('#abel').animate({left:'46%'},2000, 'easeInOutElastic');
+// $('#abel').fadeTo(2250, 1.0);
+// $('#abel').animate({left:'46%'},2000, 'easeInOutElastic');
 
 
-$('#fswd').fadeTo(3500, 1.0);
-$('#fswd').animate({top:145},2000,'easeOutBounce');
+// $('#fswd').fadeTo(3500, 1.0);
+// $('#fswd').animate({top:145},2000,'easeOutBounce');
 
 
 // GSAP ANIMATIONS
+
 
 TweenMax.to('.stacks1 ', 1.75, {marginTop:150, ease:Bounce.easeOut});
 TweenMax.to('.stacks2 ', 2.00, {marginTop:150, ease:Bounce.easeOut});
